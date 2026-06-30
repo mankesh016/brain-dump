e8d9dbf7b7cc8942664db133dddfb79ea8d65ec9
Mankesh Meena <dhanawatmankesh@gmail.com>
2026-06-30 08:03:12 +0530

    fix: add postinstall script to generate prisma client on vercel

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,26 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+
+const connectionString = process.env.DATABASE_URL!;
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const db = globalForPrisma.prisma ?? new PrismaClient();
+let db: PrismaClient;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+if (process.env.NODE_ENV === "production") {
+  const pool = new Pool({ connectionString });
+  const adapter = new PrismaPg(pool);
+  db = new PrismaClient({ adapter });
+} else {
+  if (!globalForPrisma.prisma) {
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
+    globalForPrisma.prisma = new PrismaClient({ adapter });
+  }
+  db = globalForPrisma.prisma;
+}
+
+export { db };
